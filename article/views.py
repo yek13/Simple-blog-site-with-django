c77f82ca9aa4760dd0d917c7a6b3418f257a3a6a
yek13 <yunusemrek96@gmail.com>
2019-10-29 01:36:00 +0300

    Improvements made

--- article/views.py
+++ article/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render,HttpResponse,redirect,get_object_or_404
+from .forms import ArticleForm
+from .models import Article
+from django.contrib import messages
 def index(request):
     context={
 
@@ -10,3 +12,34 @@ def index(request):
 
 def about(request):
     return render(request,"about.html")
+
+def dashboard(request):
+    articles=Article.objects.filter(author=request.user)
+    context={
+
+        "articles":articles
+    }
+    return render(request,"dashboard.html",context)
+
+def addArticle(request):
+    form =ArticleForm(request.POST or None)
+
+    if form.is_valid():
+        article=form.save(commit=False)
+
+        article.author=request.user
+        article.save()
+        messages.success(request,"Başarıyla Kaydedildi.")
+
+        return redirect("index")
+
+    context={
+        "form":form
+    }
+    return render(request,"addarticle.html",context)
+
+
+def detail(request,id):
+    article=get_object_or_404(Article,id=id)
+
+    return render(request,"detail.html",{"article":article})
