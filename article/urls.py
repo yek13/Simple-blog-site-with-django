e52eb64eae4b4a4320efbc9b483fa654f2b31eca
yek13 <yunusemrek96@gmail.com>
2019-10-30 17:52:42 +0300

    update

--- article/urls.py
+++ article/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('article/<int:id>', views.detail,name="detail"),
     path('update/<int:id>', views.updateArticle,name="update"),
     path('delete/<int:id>', views.deleteArticle,name="delete"),
+    path('', views.articles,name="articles"),
 
 
     
--- article/views.py
+++ article/views.py
@@ -4,6 +4,15 @@ from .models import Article
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+
+def articles(request):
+    keyword=request.GET.get("keyword")
+    if keyword:
+        articles=Article.objects.filter(title__contains=keyword)
+        return render(request,"articles.html",{"articles":articles})
+
+    articles=Article.objects.all()
+    return render(request,"articles.html",{"articles":articles})
 def index(request):
     context={
 
@@ -19,7 +28,7 @@ def dashboard(request):
     articles=Article.objects.filter(author=request.user)
     context={
 
-        "articles":articles
+        "articless":articles
     }
     return render(request,"dashboard.html",context)
 @login_required(login_url="user:login")
